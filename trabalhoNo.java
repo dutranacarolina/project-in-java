68e293b542de0d58cb4ddaa174c0dd951ba34068
Analina <dutranacrolina@gmail.com>
2026-03-22 15:49:18 -0300

    Creating vector, matrix, and list variables

---
+++ trabalhoNo.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class trabalhoNo {
+
+    // Adicionando os vetores, matriz e listas
+     static String nomes [] = new String[5];
+     static String materia [] = new String[5];
+     static double notas [][][] = new double[5][5][3];
+     static ArrayList<String> aprovados = new ArrayList<>(); 
+     static ArrayList<String> reprovados = new ArrayList<>(); 
+     static Scanner sc = new Scanner(System.in);
+     
+}
